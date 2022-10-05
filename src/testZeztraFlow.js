132b8865e60b478bfb3750500d5c98fa3b173e4b
Vitor Ricardo <vitorricardo@outlook.com>
2022-10-05 18:37:58 -0300

    feat: list billing rules

--- src/testZeztraFlow.js
+++ src/testZeztraFlow.js
@@ -1,4 +1,4 @@
-const { createBilling, getBillingByBillingId, getBillingByCustomId } = require('./zeztra');
+const { createBilling, getBillingByBillingId, getBillingByCustomId, listBilling } = require('./zeztra');
 
 const testZeztraFlow = async () => {
     const billingCreated = await createBilling();
@@ -13,9 +13,14 @@ const testZeztraFlow = async () => {
     const billing1 = await getBillingByBillingId(billingId);
     console.log('----------------Resultado billing1----------------');
     console.log(JSON.stringify(billing1));
+
     const billing2 = await getBillingByCustomId(customId);
     console.log('----------------Resultado billing2----------------');
     console.log('Resultado billing2', JSON.stringify(billing2));
+    
+    const listBillings = await listBilling(1, 2, 'PENDING');
+    console.log('----------------Resultado listBillings----------------');
+    console.log('Resultado listBillings', JSON.stringify(listBillings));
 }
 
 testZeztraFlow();
